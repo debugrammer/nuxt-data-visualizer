fc169bfbc31bd464fd6bab1fde52c119c44855ed
debugrammer <debugrammer@gmail.com>
2019-09-21 12:05:15 +0900

    add responsive flex size on main image

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
               <div slot="widget-content" class="index-gradient">
                 <v-container fill-height>
                   <v-layout row align-center justify-center>
-                    <v-flex xs5>
+                    <v-flex :class="imageFlexSize">
                       <v-img
                         src="/SO-Monitoring-Monitored.png"
                         alt="You are being monitored"
@@ -41,6 +41,22 @@ export default {
   data() {
     return {
       show: false
+    }
+  },
+  computed: {
+    imageFlexSize() {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs':
+          return 'xs12'
+        case 'sm':
+          return 'xs10'
+        case 'md':
+          return 'xs8'
+        case 'lg':
+          return 'xs6'
+      }
+
+      return 'xs5'
     }
   },
   mounted() {
